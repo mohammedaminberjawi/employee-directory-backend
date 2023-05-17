ca9f793098912e60826ce632545bcd010c1eb5da
Mohammed Amin Berjawi <mohammedaminberjawi@gmail.com>
2023-05-17 17:27:29 +0300

    Add employee route to app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-const express = require("express");
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+const EmployeeRouter = require('./routes/employee')
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello World!")
 });
 
-const PORT = process.env.port || 3000;
+app.use('/api/employees', EmployeeRouter);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
